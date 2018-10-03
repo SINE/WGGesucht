63f84321ec1d3cf37a16fa6fc584266b5120ba7a
SINE <sinebt@live.de>
2018-10-03 18:43:06 +0200

    WerbeSTOP v.0.1

---
+++ WerbeSTOP/WerbeSTOP.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        WG Gesucht WERBUNG STOP ALPHA
+// @namespace   github.com/SINE
+// @version     0.1
+//@downloadURL https://raw.githubusercontent.com/SINE/WGGesucht/master/WerbeSTOP/werbestop.user.js
+// ==/UserScript==
